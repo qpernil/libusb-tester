51364f37daa5ecb111e337f539bfcdcf3f7f29d1
Per Nilsson <per.nilsson@yubico.com>
2021-11-09 21:59:37 +0100

    Fix PCSC on Windows

--- libusb/PCSC.cs
+++ libusb/PCSC.cs
@@ -1,13 +1,17 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 
 namespace libusb
 {
     public class PCSC
     {
+        [UnmanagedFunctionPointer(CallingConvention.Winapi)]
         public delegate uint SCardEstablishContext(uint scope, IntPtr reserved1, IntPtr reserved2, out IntPtr ctx);
 
+        [UnmanagedFunctionPointer(CallingConvention.Winapi)]
         public delegate uint SCardReleaseContext(IntPtr ctx);
 
+        [UnmanagedFunctionPointer(CallingConvention.Winapi, CharSet = CharSet.Ansi)]
         public delegate uint SCardListReaders(IntPtr ctx, string groups, byte[] readers, ref int cb_readers);
 
         private readonly SafeNativeLibrary pcsc;
@@ -17,9 +21,9 @@ namespace libusb
 
         public const uint SCARD_SCOPE_SYSTEM = 2;
 
-        public PCSC(string libraryPath = "/System/Library/Frameworks/PCSC.framework/PCSC")
+        public PCSC()
         {
-            pcsc = new SafeNativeLibrary(libraryPath);
+            pcsc = new SafeNativeLibrary("winscard", "/System/Library/Frameworks/PCSC.framework/PCSC", "pcsclite");
             pcsc.GetExport(out establish_context);
             pcsc.GetExport(out release_context);
             pcsc.GetExport(out list_readers);
